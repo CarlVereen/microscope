13ed7b02af3b636741c15c04fe1ed6db3e5e7819
Carl Vereen <carlvereen@gmail.com>
2015-06-28 21:50:04 -0600

    Enforce post URL uniqueness

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,8 +11,13 @@ Template.postSubmit.events({
       //display the error to the user and abort
       if(error)
         return alert(error.reason);
+
+      //show this result but route anyway
+      if (result.postExists)
+        alert('This link has already been posted');
+
       Router.go('postPage', {_id: result._id});
-      
+
     });
     // post._id = Posts.insert(post);
     // Router.go('postPage', post);
